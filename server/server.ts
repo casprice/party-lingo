9e1a6d137dc9535673a2692444defa79534f9bbf
Casey Price <caseyprice217@gmail.com>
2022-02-19 02:21:29 -0800

    set up server

--- server/server.ts
+++ server/server.ts
@@ -1,32 +1,11 @@
-interface ServerToClientEvents {
-    noArg: () => void;
-    basicEmit: (a: number, b: string, c: Buffer) => void;
-    withAck: (d: string, callback: (e: number) => void) => void;
-  }
-  
-  interface ClientToServerEvents {
-    hello: () => void;
-  }
-  
-  interface InterServerEvents {
-    ping: () => void;
-  }
-  
-  interface SocketData {
-    name: string;
-    age: number;
-  }
+import express from 'express';
 
-  import { Server } from "socket.io";
+const app = express();
 
-  const io = new Server(3000, { /* options */ });
-  
-  io.on("connection", (socket) => {
-    // ...
-  });
+app.get('/', (req, res) => {
+    res.send('Casey Price is always watching you');
+})
 
-  const mode = process.env.NODE_ENV;
-const port = 8001;
-server.listen(8001, () => {
-  logger.info(`Server listening on :${port} [${mode}]`);
-});
+app.listen(8000, () => {
+    console.log('The application is listening on port 8000!');
+})
