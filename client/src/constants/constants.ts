cf0e9a3a707dee2361e8c4689a3d005ada7d4e1c
angelaliuu <anliu@ucsd.edu>
2022-02-20 03:46:43 -0800

    restore 3 lives

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,6 +1,6 @@
 export const mockPlayers = [
-  { idx: 0, name: "Jaden", lives: [1], deads: [], active: true },
-  { idx: 1, name: "Angela", lives: [1], deads: [], active: false },
-  { idx: 2, name: "Casey", lives: [1], deads: [], active: false },
-  { idx: 3, name: "Michele", lives: [1], deads: [], active: false },
+  { idx: 0, name: "Jaden", lives: [1, 1, 1], deads: [], active: true },
+  { idx: 1, name: "Angela", lives: [1, 1, 1], deads: [], active: false },
+  { idx: 2, name: "Casey", lives: [1, 1, 1], deads: [], active: false },
+  { idx: 3, name: "Michele", lives: [1, 1, 1], deads: [], active: false },
 ];
